0b312d828f5598377fe0311e5adc11c9f1eb66cd
radhapaswan0625@gmail.com <paswanradha812@gmail.com>
2026-06-06 14:25:00 +0545

    remove pycache and clean repo

--- __pycache__/book.py
+++
@@ -1,6 +0,0 @@
-class Book:
-    def __init__(self, book_id, title, author, quantity):
-        self.book_id = book_id
-        self.title = title
-        self.author = author
-        self.quantity = quantity
